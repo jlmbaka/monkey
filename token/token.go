677628ecfff0db8f3e14fb75312c1f6c27a770c8
Jean-Louis <jeanlouis.mbaka@gmail.com>
2020-06-26 21:48:08 +0100

    Initial commit:   tests tokens  (=+(){},;)

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// IDENTIIFERS  +  LITERALS
+	IDENT = "ident"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	//  Delimeters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// KEYWORDS
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
